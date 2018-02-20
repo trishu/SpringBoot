31f6e466540eb742cb7c6a42385f1b6dbfec6d33
Trishu <a.trishu@gmail.com>
2018-02-20 13:34:04 +0530

    course api initial files commit

---
+++ course-api/src/main/java/io/trishu/springbootstarter/topic/TopicService.java
@@ -0,0 +1,29 @@
+package io.trishu.springbootstarter.topic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
+				new Topic("java", "Java Course", "java course description"),
+				new Topic("spring", "Spring Course", "Spring course description"),
+				new Topic("javascript", "Javascript Course", "Javascript course description")
+				));
+	
+	public List<Topic> getAllTopics(){
+		return topics;
+	} 
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+}
