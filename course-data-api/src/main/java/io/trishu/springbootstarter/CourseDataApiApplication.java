8e7d53f2cf1d43eb2ffa8f62dee1046f22a5f5b2
Trishu <a.trishu@gmail.com>
2018-02-21 13:43:46 +0530

    first commit for course api with jpa integration

---
+++ course-data-api/src/main/java/io/trishu/springbootstarter/CourseDataApiApplication.java
@@ -0,0 +1,12 @@
+package io.trishu.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseDataApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseDataApiApplication.class, args);
+	}
+}
